4320915a6bd3419d14ba652db67ca765d6408da0
anagarango <anitag.arango@icloud.com>
2024-06-14 14:29:07 -0700

    test async

--- src/app/dumb/page.tsx
+++ src/app/dumb/page.tsx
@@ -1,29 +1,29 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
- 
-export default function Home() {
-  const [answerWord, setAnswerWord] = useState<string>("")
 
+export default function Home() {
+  const [answerWord, setAnswerWord] = useState<string>("");
 
   const fetchWord = async () => {
     try {
-      const response : Response = await fetch("/api");
+      const response = await fetch("/api", {
+        cache: "no-store" // Ensures the response is not cached
+      });
       const data = await response.json();
       setAnswerWord(data.word.toUpperCase());
-      console.log(data)
+      console.log(data);
     } catch (error) {
       console.error('Error fetching the word:', error);
     }
-  }
+  };
 
-
-  useEffect(()=>{
-    fetchWord()
-  },[])  
+  useEffect(() => {
+    fetchWord();
+  }, []);
 
   return (
     <main className="main">
-        <button onClick={()=>fetchWord()}>{answerWord}</button>
+      <button onClick={() => fetchWord()}>{answerWord}</button>
     </main>
   );
 }
