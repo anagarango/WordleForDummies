44aeed26b75568d42e36b31e3fdda42deb1dae7e
anagarango <anitag.arango@icloud.com>
2024-06-14 14:24:53 -0700

    test async

---
+++ src/app/dumb/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { useEffect, useState } from "react";
+ 
+export default function Home() {
+  const [answerWord, setAnswerWord] = useState<string>("")
+
+
+  const fetchWord = async () => {
+    try {
+      const response : Response = await fetch("/api");
+      const data = await response.json();
+      setAnswerWord(data.word.toUpperCase());
+      console.log(data)
+    } catch (error) {
+      console.error('Error fetching the word:', error);
+    }
+  }
+
+
+  useEffect(()=>{
+    fetchWord()
+  },[])  
+
+  return (
+    <main className="main">
+        <button onClick={()=>fetchWord()}>{answerWord}</button>
+    </main>
+  );
+}
